fbd11b279929cec1810fedd57e156eb32303a4d4
rohit42i <rohitbaswaraj@gmail.com>
2026-08-12 05:38:15 +0530

    Enable global persistent theme toggle and light mode

--- main.jsx
+++ main.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import ThemeToggle from './ThemeToggle.jsx';
 import './index.css';
 import './ui-polish.css';
 import './admin-polish.css';
 import './mihon-reader-polish.css';
+import './light-mode.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <ThemeToggle />
   </React.StrictMode>
 );
