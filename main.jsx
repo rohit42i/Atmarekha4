8e893cc7087c5f749d874349f7d52995d2f8d80e
rohit42i <rohitbaswaraj@gmail.com>
2026-08-13 05:28:09 +0530

    Mount global Google auth and profile experience

--- main.jsx
+++ main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import ThemeToggle from './ThemeToggle.jsx';
+import UserAuth from './UserAuth.jsx';
 import './index.css';
 import './ui-polish.css';
 import './admin-polish.css';
@@ -11,10 +12,12 @@ import './admin-link-fix.css';
 import './notification-fix.js';
 import './reader-performance.css';
 import './reader-performance.js';
+import './user-auth.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <ThemeToggle />
+    <UserAuth />
   </React.StrictMode>
 );
