52d440556064b46ace8b5e36a20e8c1a4bb49120
rohit42i <rohitbaswaraj@gmail.com>
2026-08-13 06:03:01 +0530

    Add redesigned editable profile

--- main.jsx
+++ main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import ThemeToggle from './ThemeToggle.jsx';
 import UserAuth from './UserAuth.jsx';
+import ProfileV2 from './ProfileV2.jsx';
 import ReaderBookmark from './ReaderBookmark.jsx';
 import './index.css';
 import './ui-polish.css';
@@ -15,12 +16,14 @@ import './reader-performance.css';
 import './reader-performance.js';
 import './user-auth.css';
 import './user-auth-layout.css';
+import './profile-v2.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <ThemeToggle />
     <UserAuth />
+    <ProfileV2 />
     <ReaderBookmark />
   </React.StrictMode>
 );
