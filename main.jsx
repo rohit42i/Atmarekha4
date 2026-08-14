81e0ae43c34bd634420732af8a2581389f442eb9
rohit42i <rohitbaswaraj@gmail.com>
2026-08-14 22:18:33 +0530

    remove obsolete chapter style import

--- main.jsx
+++ main.jsx
@@ -34,7 +34,6 @@ import './enhanced-comments.css';
 import './public-profile.css';
 import './premium-typography.css';
 import './final-polish.css';
-import './chapter-ui-final.css';
 import './appearance-polish.css';
 import './rating-upgrade.js';
 
