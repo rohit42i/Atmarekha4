345bb918e3e28d3b426b376b85cfcfa3d9e2e146
rohit42i <rohitbaswaraj@gmail.com>
2026-08-17 08:28:07 +0530

    Load additive admin overview UI polish

--- main.jsx
+++ main.jsx
@@ -19,6 +19,7 @@ import './ui-polish.css';
 import './interaction-polish.css';
 import './admin-polish.css';
 import './admin-upgrade.css';
+import './admin-overview-upgrade.css';
 import './mihon-reader-polish.css';
 import './admin-link-fix.css';
 import './notification-fix.js';
