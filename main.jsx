d22ae7de72af844d3dda41a70d131831008d56fe
rohit42i <rohitbaswaraj@gmail.com>
2026-08-29 14:06:19 +0530

    Unhide Group Chat while keeping membership hidden

--- main.jsx
+++ main.jsx
@@ -66,4 +66,4 @@ function GroupChatLauncherGate(){
   return <GroupChatLauncher user={user}/>;
 }
 
-createRoot(document.getElementById('root')).render(<React.StrictMode><AtmaLoader/><App/><UserAuth/><ReaderBookmark/><ReadingHistoryTracker/><AuthGate/><ChapterCompletionPrompt/><CommunityPage/><CommunityAdmin/><EnhancedComments/><PublicProfile/><Membership/><ChapterAccessGuard/><ThemeToggle/></React.StrictMode>);
+createRoot(document.getElementById('root')).render(<React.StrictMode><AtmaLoader/><App/><UserAuth/><ReaderBookmark/><ReadingHistoryTracker/><AuthGate/><ChapterCompletionPrompt/><CommunityPage/><CommunityAdmin/><EnhancedComments/><PublicProfile/><Membership/><GroupChatLauncherGate/><GroupChat/><ChapterAccessGuard/><ThemeToggle/></React.StrictMode>);
